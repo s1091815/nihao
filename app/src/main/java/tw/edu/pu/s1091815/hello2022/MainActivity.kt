a53eb798e4809add40fa23189e5fe21e2694aeb3
s1091815 <s1091815@gm.pu.edu.tw>
2022-03-22 11:35:29 +0800

    併後，再次修改txv顯示的文字

--- app/src/main/java/tw/edu/pu/s1091815/hello2022/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/s1091815/hello2022/MainActivity.kt
@@ -6,21 +6,7 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        var txv: TextView = findViewById<TextView>(R.id.txv)
-        txv.text = "Hello tcyang"
-        TestDEV()
+        super.onCreate(savedInstanceState) setContentView (R.layout.activity_main)
+        var txv: TextView = findViewById<TextView>(R.id.txv)//txv.text = "Hello tcyang"//TestDEV()//TestMaster()txv.text = "嗨，我是楊子青"}
     }
-
-    fun TestDEV(){
-        var txv:TextView = findViewById<TextView>(R.id.txv)
-        txv.text = "Dev分支"
-
-        TestMaster()
-    }
-
-    fun TestMaster(){
-        var txv:TextView = findViewById<TextView>(R.id.txv)
-        txv.text = "Master分支"}
 }
